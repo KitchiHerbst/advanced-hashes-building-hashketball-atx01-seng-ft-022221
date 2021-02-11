9233dfa92e9fe67562838c539b2363eaded333e2
Triston Herbst <tristonherbst97@gmail.com>
2021-02-11 15:07:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,7 +6,7 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        {:player_name => "Alan Anderson", :number => , :shoe => , :points => ,
+        {:player_name => "Alan Anderson", :number => 0 , :shoe => , :points => ,
         :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
         
         },
