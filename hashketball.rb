22aaa5ea15e86ae93a82c438fe859bca473188f6
Triston Herbst <tristonherbst97@gmail.com>
2021-02-11 15:06:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,6 +30,26 @@ def game_hash
       :team_name => "",
       :colors => [],
       :players => [
+        {:player_name => "Alan Anderson", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
+        
+        },
+        {:player_name => "Reggie Evans", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
+        
+        },
+        {:player_name => "Brook Lopez", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
+        
+        },
+        {:player_name => "Mason Plumlee", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
+        
+        },
+        {:player_name => "Jason Terry", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
+        
+        }
         ]
     }
   }
