ef5b0d9f52f5c6cc8049526749831cbbcd5b191a
Triston Herbst <tristonherbst97@gmail.com>
2021-02-11 15:04:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,9 +6,30 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        {:player_name => "Alan Anderson", :number => , :shoe => , :points => , 
+        {:player_name => "Alan Anderson", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
+        ]
+     },
+        {:player_name => "Reggie Evans", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
         ]
     },
+        {:player_name => "Brook Lopez", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
+        ]
+    },
+        {:player_name => "Mason Plumlee", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
+        ]
+    },
+        {:player_name => "Alan Anderson", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
+        ]
+    },
+        {:player_name => "Alan Anderson", :number => , :shoe => , :points => ,
+        :rebounds => , :assists => , :steals => , :blocks => , :slam_dunks => ,
+        ]
+    },  
     :away => {
       :team_name => "",
       :colors => [],
